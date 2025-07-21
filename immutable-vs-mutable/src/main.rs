78be90be05a4ad102f757d8849774d65a0dc6f97
Joe Kim <joesm.genie@gmail.com>
2025-07-21 14:27:33 +0700

    immutable vs mutable

---
+++ immutable-vs-mutable/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    // Immutable variable
+    // let crabby_energy = 100;
+    // println!("{}", crabby_energy);
+    
+    // Mutable variable
+    let mut crabby_energy = 100;
+    println!("{}", crabby_energy);
+    crabby_energy = 90;
+    println!("{}", crabby_energy);
+}
